c06dcd17bbf29b65cadc3710e5694c080714eb56
Avek.io tech <tech@avek.io>
2022-09-30 23:37:07 +0300

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 declare(strict_types=1);
 
+use App\Http\Controllers\BotStatisticsController;
+use App\Http\Controllers\FlowBlockStatisticsController;
+use App\Http\Controllers\FlowCommandsStatisticsController;
+use App\Http\Controllers\FlowTelegramUsersStatisticsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => 'test');
+Route::get('/statistics/bot', [BotStatisticsController::class, 'index']);
+Route::get('/statistics/flow-commands', [FlowCommandsStatisticsController::class, 'index']);
+Route::get('/statistics/flow-block', [FlowBlockStatisticsController::class, 'index']);
+Route::get('/statistics/flow-telegram-users', [FlowTelegramUsersStatisticsController::class, 'index']);
