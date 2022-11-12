81daced62dbbaf6c420473ce65629969242020e4
“Avek.io <tech@avek.io>
2022-11-12 12:55:24 +0200

    Fix typo

--- app/Http/Responses/NotFoundResponse.php
+++ app/Http/Responses/NotFoundResponse.php
@@ -8,10 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 class NotFoundResponse extends Response
 {
     private const MESSAGE = 'Not found';
-    protected const CODE = Response::HTTP_NOT_FOUND;
 
     public function __construct()
     {
-        parent::__construct(self::MESSAGE, self::CODE);
+        parent::__construct(self::MESSAGE, Response::HTTP_NOT_FOUND);
     }
 }
