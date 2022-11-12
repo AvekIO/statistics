8ca536ef78135ae57fe27247588f43fcf0513c4d
“Avek.io <tech@avek.io>
2022-11-12 17:25:04 +0200

    Refactor health check service

--- app/Services/HealthCheckService.php
+++ app/Services/HealthCheckService.php
@@ -4,30 +4,39 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Exception;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Cache\CacheManager;
+use Illuminate\Database\DatabaseManager;
 
 class HealthCheckService
 {
+    private const CACHE_CHECK_KEY = 'cache_health_check';
+    private const CACHE_CHECK_TTL = 30;
+
     private bool $status = true;
+
+    public function __construct(private readonly DatabaseManager $db, private readonly CacheManager $cache)
+    {
+    }
 
     public function getStatus(): array
     {
         return [
-            'mysql_status' => $this->getMysqlStatus(),
-            'redis_status' => $this->getRedisStatus(),
+            'db_status' => $this->getDbStatus(),
+            'cache_status' => $this->getCacheStatus(),
             'status' => $this->status,
         ];
     }
 
-    private function getMysqlStatus(): bool|string
+    private function getDbStatus(): bool|string
     {
-        return $this->wrapCheck(fn () => (bool) DB::connection()->getPDO());
+        return $this->wrapCheck(fn () => (bool) $this->db->connection()->getPDO());
     }
 
-    private function getRedisStatus(): bool|string
+    private function getCacheStatus(): bool|string
     {
-        return $this->wrapCheck(fn () => (bool) Redis::connection('cache'));
+        return $this->wrapCheck(fn () =>
+            $this->cache->remember(self::CACHE_CHECK_KEY, self::CACHE_CHECK_TTL, fn () => true)
+        );
     }
 
     private function wrapCheck(callable $func): bool|string
