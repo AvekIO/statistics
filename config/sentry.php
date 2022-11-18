a1b6633c124c313722d16e0e1f1746c4b06ea30d
“Avek.io <tech@avek.io>
2022-11-18 10:06:44 +0200

    Refactor sentry config

--- config/sentry.php
+++ config/sentry.php
@@ -2,8 +2,7 @@
 declare(strict_types=1);
 
 return [
-    'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
-    'environment' => env('SENTRY_ENVIRONMENT'),
+    'dsn' => env('SENTRY_LARAVEL_DSN'),
     'breadcrumbs' => [
         'logs' => true,
         'sql_queries' => true,
@@ -12,14 +11,13 @@ return [
         'command_info' => true,
     ],
     'tracing' => [
-        'queue_job_transactions' => env('SENTRY_TRACE_QUEUE_ENABLED', false),
+        'queue_job_transactions' => false,
         'queue_jobs' => true,
         'sql_queries' => true,
         'sql_origin' => true,
         'views' => true,
         'default_integrations' => true,
     ],
-    'send_default_pii' => env('SENTRY_SEND_DEFAULT_PII', false),
-    'traces_sample_rate' => (float)(env('SENTRY_TRACES_SAMPLE_RATE', 0.0)),
-    'controllers_base_namespace' => env('SENTRY_CONTROLLERS_BASE_NAMESPACE', 'App\\Http\\Controllers'),
+    'send_default_pii' => false,
+    'traces_sample_rate' => (float) env('SENTRY_TRACES_SAMPLE_RATE'),
 ];
