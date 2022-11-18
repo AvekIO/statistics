29bc33f229035d03e00960fbd3cab92e5763f962
“Avek.io <tech@avek.io>
2022-11-18 05:57:54 +0200

    Refactor flow commands

--- app/DTO/FlowCommandsStatisticsDto.php
+++ app/DTO/FlowCommandsStatisticsDto.php
@@ -8,7 +8,7 @@ class FlowCommandsStatisticsDto
     public function __construct(
         public readonly int $flowId,
         public readonly ?int $commandId,
-        public readonly ?string $createdAtFrom,
-        public readonly ?string $createdAtTo
+        public readonly ?string $triggeredAtFrom,
+        public readonly ?string $triggeredAtTo
     ) {}
 }
--- app/Models/FlowCommandsStatistics.php
+++ app/Models/FlowCommandsStatistics.php
@@ -5,7 +5,11 @@ namespace App\Models;
 
 class FlowCommandsStatistics extends AbstractModel
 {
-    public const UPDATED_AT = null;
+    public $timestamps = false;
 
     protected $table = 'flow_commands_statistics';
+
+    protected $casts = [
+        'triggered_at'  => 'datetime:Y-m-d H:i:s',
+    ];
 }
